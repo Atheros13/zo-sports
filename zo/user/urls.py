686c7d4de243d4f8951e5351b8fa8db33a5fbd05
Atheros13 <45272000+Atheros13@users.noreply.github.com>
2019-09-08 08:31:06 +1200

    Update urls.py

--- zo/user/urls.py
+++ zo/user/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('settings/', views.settings, name='profile_settings'),
-    path('confirm_user_signup/<str:email>/<str:firstname>/<str:surname>/<str:message>', 
+    path('confirm_user_signup/<str:email>/<str:firstname>/<str:surname>/<str:phone>/<str:message>', 
          views.confirm_user_signup, name='confirm_user_signup'),
     ]
